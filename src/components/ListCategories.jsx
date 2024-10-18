bc6276e3bd3a33584c35e1821473dcd28e40cc78
putrafir <141028423+putrafir@users.noreply.github.com>
2024-10-18 22:50:34 +0700

    axios get categories

--- src/components/ListCategories.jsx
+++ src/components/ListCategories.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 import { Col } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constans";
 
 export default class ListCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((res) => {
+        const categories = res.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    console.log("categories: ", this.state.categories);
+
     return (
       <Col md={2} mt="2">
         <h4>
