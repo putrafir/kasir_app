0946f5957f16f6916551b8bb17727b5ed25b46e1
putrafir <141028423+putrafir@users.noreply.github.com>
2024-11-23 14:58:51 +0700

    ok

--- src/components/TotalBayar.jsx
+++ src/components/TotalBayar.jsx
@@ -1,16 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import { API_URL } from "../utils/constans";
+import { useNavigate } from "react-router-dom";
 
-export default class TotalBayar extends Component {
-  submitTotalBayar = (TotalBayar) => {
+const TotalBayar = ({ keranjangs }) => {
+  const navigate = useNavigate();
+
+  const submitTotalBayar = (TotalBayar) => {
     const pesanan = {
       total_bayar: TotalBayar,
-      menus: this.props.keranjangs,
+      menus: keranjangs,
     };
 
     console.log("Request data:", pesanan);
@@ -18,39 +21,40 @@ export default class TotalBayar extends Component {
     axios
       .post(API_URL + "pesanans", pesanan)
       .then((res) => {
-        this.props.history.push("/success");
+        navigate("/success");
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
 
-  render() {
-    const TotalBayar = this.props.keranjangs.reduce(function (result, item) {
-      return result + item.total_harga;
-    }, 0);
-    return (
-      <div className="fixed-bottom">
-        <Row>
-          <Col md={{ span: 3, offset: 9 }} className="px-4">
-            <h4>
-              Total harga :
-              <strong className="float-right">
-                Rp. {numberWithCommas(TotalBayar)}
-              </strong>
-            </h4>
-            <Button
-              variant="primary"
-              block
-              className="mb-2 mt-4 mr-2"
-              size="lg"
-              onClick={() => this.submitTotalBayar(TotalBayar)}
-            >
-              <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
-            </Button>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+  const TotalBayar = keranjangs.reduce((result, item) => {
+    return result + item.total_harga;
+  }, 0);
+
+  return (
+    <div className="fixed-bottom">
+      <Row>
+        <Col md={{ span: 3, offset: 9 }} className="px-4">
+          <h4>
+            Total harga :
+            <strong className="float-right">
+              Rp. {numberWithCommas(TotalBayar)}
+            </strong>
+          </h4>
+          <Button
+            variant="primary"
+            block
+            className="mb-2 mt-4 mr-2"
+            size="lg"
+            onClick={() => submitTotalBayar(TotalBayar)}
+          >
+            <FontAwesomeIcon icon={faShoppingCart} /> <strong>Bayar</strong>
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default TotalBayar;
